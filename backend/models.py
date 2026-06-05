205d083c6bda08ebd118ea5ea2f40dfbedf62ea5
Abhishek Bante <abhibante003@gmail.com>
2026-06-05 17:29:18 +0530

    Prepare for Render deployment

--- backend/models.py
+++ backend/models.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+import os
 
-DATABASE_URL = "sqlite:///./ai_cfo.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./ai_cfo.db"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
