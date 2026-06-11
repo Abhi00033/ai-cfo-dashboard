434f7f94ea83a057f7e4faa924b6da8774002b59
Abhishek Bante <abhibante003@gmail.com>
2026-06-12 04:04:29 +0530

    Name change

--- frontend/config.js
+++ frontend/config.js
@@ -1,12 +1,14 @@
 const CONFIG = {
-
     API_BASE_URL: "https://ai-cfo-backend-fq1a.onrender.com",
+
 
     APP_NAME: "AI CFO",
 
-    TEAM_NAME: "Smart Solutions Team",
 
-    CREATED_BY: "Smart Solutions Team",
+    TEAM_NAME: "Smart Solution Team",
+
+
+    CREATED_BY: "Smart Solution Team",
 
     ROLE: "Project Developer"
 };
